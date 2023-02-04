625128b26feee9bfaa226f4583d97784c665038d
lesbianboy <the.god.of.thander.omega@gmail.com>
2023-02-04 15:41:47 +0600

    upd1.0

--- lib/pages/category.page.dart
+++ lib/pages/category.page.dart
@@ -139,7 +139,7 @@ class _CategoryPageState extends State<CategoryPage> {
                                 }
                             );
                           },
-                          icon: Icon(Iconsax.trash)
+                          icon: const Icon(Iconsax.trash)
                       ),
                       const IconButton(onPressed: null, icon: Icon(null)),
                       IconButton(
